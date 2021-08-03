dc7a55f116a32d6805ce86c9f437ee9dece4286e
phoenix <62538135+phoenix-1708@users.noreply.github.com>
2021-08-03 14:06:07 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -11,7 +11,8 @@ echo "Cloning dependencies"
 # git clone --depth=1 https://github.com/ArrowOS-Devices/android_kernel_xiaomi_sweet.git -b arrow-11.0
 git clone --depth=1 https://github.com/crdroidandroid/android_kernel_xiaomi_sweet.git -b 11.0
 cd android_kernel_xiaomi_sweet
-git clone --depth=1 https://github.com/xyz-prjkt/xRageTC-clang.git clang
+# git clone --depth=1 https://github.com/xyz-prjkt/xRageTC-clang.git clang
+git clone --depth=1 https://github.com/Haseo97/Avalon-Clang-12.0.0.git clang
 git clone https://github.com/phoenix-1708/Anykernel3-Tissot.git  --depth=1 AnyKernel
 git clone https://github.com/fabianonline/telegram.sh.git  -b master
 KERNEL_DIR=$(pwd)
