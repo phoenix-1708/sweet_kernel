10293d17bd50d221ab74db07b4a14bb6f34f6267
phoenix <62538135+phoenix-1708@users.noreply.github.com>
2021-08-04 10:08:11 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+# Colors
+green='\033[01;32m'
+red='\033[01;31m'
+blink_red='\033[05;31m'
+cyan='\033[0;36m'
+yellow='\033[0;33m'
+blue='\033[0;34m'
+default='\033[0m'
+
 # mkdir -p /tmp/rom # Where to sync source
 # cd /tmp/rom
 git clone https://github.com/phoenix-1708/scripts.git && cd scripts && bash setup/android_build_env.sh && cd ..
@@ -14,63 +23,44 @@ cd android_kernel_xiaomi_sweet
 
 # git clone --depth=1 https://github.com/xyz-prjkt/xRageTC-clang.git clang
 # git clone --depth=1 https://github.com/Haseo97/Avalon-Clang-12.0.0.git clang
-
-
-git clone https://github.com/phoenix-1708/xRageTC-clang.git clang
-git clone https://github.com/phoenix-1708/Anykernel3-Tissot.git  --depth=1 AnyKernel
+# git clone https://github.com/phoenix-1708/Anykernel3-Tissot.git  --depth=1 AnyKernel
+git clone https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86 clang
+cd clang
+git clone https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9
+git clone https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9
+cd ..
 git clone https://github.com/fabianonline/telegram.sh.git  -b master
 KERNEL_DIR=$(pwd)
 REPACK_DIR="${KERNEL_DIR}/AnyKernel"
 IMAGE="${KERNEL_DIR}/out/arch/arm64/boot/Image.gz"
-DTB_T="${KERNEL_DIR}/out/arch/arm64/boot/dts/qcom/msm8953-qrd-sku3-tissot-treble.dtb"
-DTB="${KERNEL_DIR}/out/arch/arm64/boot/dts/qcom/msm8953-qrd-sku3-tissot-nontreble.dtb"
 SEND_DIR="${KERNEL_DIR}/telegram.sh"
 BRANCH="$(git rev-parse --abbrev-ref HEAD)"
-export PATH="$(pwd)/clang/bin:$PATH"
+PATH="$(pwd)/clang/clang-r416183c/bin:$(pwd)/clang/aarch64-linux-android-4.9/bin:$(pwd)/clang/arm-linux-androideabi-4.9/bin:${PATH}"
 export ARCH=arm64
 export KBUILD_BUILD_USER=phoenix-1708
 export KBUILD_BUILD_HOST=cirrusci
+export TARGET_KERNEL_CLANG_COMPILE=true
+echo -e "$green***********************************************"
+echo  "          Compiling Phoenix Kernel              "
+echo -e "***********************************************"
 # Compile plox
 function compile() {
     make -j$(nproc --all) O=out ARCH=arm64 sweet_user_defconfig
     make -j$(nproc --all) O=out \
                           ARCH=arm64 \
                           CC=clang \
-                          CROSS_COMPILE=aarch64-linux-gnu- \
-                          CROSS_COMPILE_ARM32=arm-linux-gnueabi- \
+                          CLANG_TRIPLE=aarch64-linux-gnu- \
+                          CROSS_COMPILE=aarch64-linux-android- \
+                          CROSS_COMPILE_ARM32=arm-linux-androideabi- \
 
-    cd $REPACK_DIR
-    mkdir kernel
-    mkdir dtb-treble
-    mkdir dtb-nontreble
-
-    if ! [ -a "$IMAGE" ]; then
-        exit 1
-        echo "There are some issues with image"
-    fi
-    cp $IMAGE $REPACK_DIR/kernel/
-
-    if ! [ -a "$DTB" ]; then
-        exit 1
-        echo "There are some issues dtb "
-    fi
-    cp $DTB $REPACK_DIR/dtb-nontreble/
-
-    if ! [ -a "$DTB_T" ]; then
-        exit 1
-        echo "There are some issues dtb treble"
-    fi
-    cp $DTB_T $REPACK_DIR/dtb-treble/
 }
 # Zipping
-function zipping() {
-    cd $REPACK_DIR || exit 1
-    zip -r9 PhoenixKernel_NonOC.zip *
-    cd $SEND_DIR   || exit 1
-    echo "Changing Dir to Send FIle"
-    ./telegram -t 1858827137:AAFZVaKOjAhjVyCXfiGgL-SK6dp7_lILZIE -c -1001521910426 -f $REPACK_DIR/PhoenixKernel_NonOC.zip "Zip Sent through Cirrus CI"
+KERNEL_DIR=$(pwd)
+IMAGE="${KERNEL_DIR}/out/arch/arm64/boot/Image.gz"
+cd telegram.sh
+./telegram -t 1858827137:AAFZVaKOjAhjVyCXfiGgL-SK6dp7_lILZIE -c -509071822 -f $IMAGE
+echo "Zip Sent through GithubActions"
    #curl --upload-file ./PhoenixKernel_NonOC.zip https://transfer.sh/PhoenixKernel_NonOC.zip
-}
-compile
-zipping
-echo "Finished"
+echo -e "$cyan**************************************************"
+echo  "                 Build Completed                    "
+echo -e "***********************************************$default"
