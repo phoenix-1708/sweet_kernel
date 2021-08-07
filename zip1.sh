6098c95b180baf3a9f356b504cf208351466c88e
phoenix <62538135+phoenix-1708@users.noreply.github.com>
2021-08-07 07:45:30 +0530

    Update zip1.sh

--- zip1.sh
+++ zip1.sh
@@ -5,20 +5,20 @@ git clone https://github.com/phoenix-1708/Anykernel.git -b master
 KERNEL_DIR=$(pwd)
 IMAGE="/home/runner/work/sweet_kernel/sweet_kernel/phoenix/android_kernel_xiaomi_sweet/out/arch/arm64/boot/Image.gz"
 ZIP="/home/runner/work/sweet_kernel/sweet_kernel/phoenix/android_kernel_xiaomi_sweet/Anykernel"
-SEND_DIR="/home/runner/work/sweet_kernel/sweet_kernel/phoenix/android_kernel_xiaomi_sweet/telegram.sh"
+SEND_DIR="/home/runner/work/sweet_kernel/sweet_kernel/phoenix/android_kernel_xiaomi_sweet/Anykernel/PhoenixKernel.zip"
 BRANCH="$(git rev-parse --abbrev-ref HEAD)"
 
 cp $IMAGE $ZIP
 
 cd Anykernel
-
+echo "zipping"
 zip -r9 PhoenixKernel.zip *
-
+echo "zipping Finished"
 git clone https://github.com/fabianonline/telegram.sh.git -b master telegram.sh
 
 cd telegram.sh
 
-./telegram -t 1858827137:AAFZVaKOjAhjVyCXfiGgL-SK6dp7_lILZIE -c -509071822 -f $ZIP/PhoenixKernel.zip "Zip sent through GithubActions"
+./telegram -t 1858827137:AAFZVaKOjAhjVyCXfiGgL-SK6dp7_lILZIE -c -509071822 -f $SEND_DIR "Zip sent through GithubActions"
 echo "Zip Sent through GithubActions"
    #curl --upload-file ./PhoenixKernel_NonOC.zip https://transfer.sh/PhoenixKernel_NonOC.zip
 echo -e "$cyan**************************************************"
