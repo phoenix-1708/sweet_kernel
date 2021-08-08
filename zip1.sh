c5d45b2a790e677b0018a89e3d61a40fb98d76c2
phoenix <62538135+phoenix-1708@users.noreply.github.com>
2021-08-08 08:57:58 +0530

    Update zip1.sh

--- zip1.sh
+++ zip1.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
 git clone https://github.com/phoenix-1708/Anykernel.git -b master
-git clone https://github.com/fabianonline/telegram.sh.git -b master telegram
+git clone https://github.com/fabianonline/telegram.sh.git -b master
 
 KERNEL_DIR=$(pwd)
 IMAGE="/home/runner/work/sweet_kernel/sweet_kernel/phoenix/android_kernel_xiaomi_sweet/out/arch/arm64/boot/Image.gz"
 ZIP="/home/runner/work/sweet_kernel/sweet_kernel/phoenix/android_kernel_xiaomi_sweet/Anykernel"
-SEND_DIR="/home/runner/work/sweet_kernel/sweet_kernel/phoenix/android_kernel_xiaomi_sweet/telegram"
-ZIP_FINAL="/home/runner/work/sweet_kernel/sweet_kernel/phoenix/android_kernel_xiaomi_sweet/Anykernel/PhoenixKernel.zip"
+SEND_DIR="/home/runner/work/sweet_kernel/sweet_kernel/phoenix/android_kernel_xiaomi_sweet/telegram.sh"
 BRANCH="$(git rev-parse --abbrev-ref HEAD)"
 
 cp $IMAGE $ZIP
@@ -17,15 +16,16 @@ cd Anykernel
 echo "zipping Started"
 zip -r9 PhoenixKernel.zip *
 echo "zipping Finished"
+ZIP_FINAL="/home/runner/work/sweet_kernel/sweet_kernel/phoenix/android_kernel_xiaomi_sweet/Anykernel/PhoenixKernel.zip"
 ls
-echo "Copying to telegram folder"
+echo "Copying to telegram.sh folder"
 $(pwd)
-cp $HOME/runner/work/sweet_kernel/sweet_kernel/phoenix/android_kernel_xiaomi_sweet/Anykernel/PhoenixKernel.zip/PhoenixKernel.zip $HOME/runner/work/sweet_kernel/sweet_kernel/phoenix/android_kernel_xiaomi_sweet/telegram
-echo "Copied to telegram folder"
-echo "changing to telegram dir"
+cp $ZIP_FINAL $SEND_DIR
+echo "Copied to telegram.sh folder"
+echo "changing to telegram.sh dir"
 cd ..
-cd telegram
-echo "changed to telegram dir"
+cd telegram.sh
+echo "changed to telegram.sh dir"
 ls
 ./telegram -t 1858827137:AAFZVaKOjAhjVyCXfiGgL-SK6dp7_lILZIE -c -509071822 -f PhoenixKernel.zip "Zip sent through GithubActions"
 echo "Zip Sent through GithubActions"
